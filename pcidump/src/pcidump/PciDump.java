f15ab5ff11d67c46e712613d99f3d051a24295b3
Ilya Manusov ( IC Book ) <Manusov1969@gmail.com>
2019-09-21 11:24:37 +0300

    v0.31 update database

--- pcidump/src/pcidump/PciDump.java
+++ pcidump/src/pcidump/PciDump.java
@@ -31,7 +31,7 @@ public class PciDump
 {
 
 private static final String MSG_ABOUT =
-    "PCI binary image analyser v0.30. (C)2019 IC Book Labs.";
+    "PCI binary image analyser v0.31. (C)2019 IC Book Labs.";
 
 private static final String MSG_LINE_ERROR =
     "Invalid command line, usage:" +
